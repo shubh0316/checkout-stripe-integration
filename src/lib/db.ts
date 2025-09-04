14555c2be0335624c14f55153bdc3d61c59462b3
Shubhanker Saxena <65784896+shubh0316@users.noreply.github.com>
2025-09-04 12:56:55 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/checkout";
+const MONGODB_URI = process.env.MONGODB_URL || "mongodb+srv://shubhankersaxena5_db_user:BlSyNoVODv5o0NfN@checkout.xipptam.mongodb.net/";
 
 let hasLoggedConnected = false;
 
