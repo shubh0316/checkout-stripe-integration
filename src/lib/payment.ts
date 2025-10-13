73d2786e334f4aeedd6d55e9acbe18662e7b57eb
shubhanker <ankursaxena347@gmail.com>
2025-10-13 15:18:40 +0530

    fixed: payment

--- src/lib/payment.ts
+++ src/lib/payment.ts
@@ -44,55 +44,8 @@ export async function generatePaymentLink(data: PaymentLinkData): Promise<string
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: [
-        // Cards & Digital Wallets
-        "card",
-        "paypal",
-        "link",
-        "revolut_pay",
-        "amazon_pay",
-        
-        // Buy Now Pay Later (BNPL)
-        "klarna",
-        "affirm",
-        "afterpay_clearpay",
-        
-        // European Banking
-        "sepa_debit",
-        "giropay",          // Germany
-        "sofort",           // Germany & Austria
-        "bancontact",       // Belgium
-        "eps",              // Austria
-        "ideal",            // Netherlands
-        "p24",              // Poland (Przelewy24)
-        "blik",             // Poland
-        "multibanco",       // Portugal
-        "bacs_debit",       // UK Direct Debit
-        
-        // Nordic Banking
-        "mobilepay",        // Denmark
-        "swish",            // Sweden
-        "twint",            // Switzerland
-        
-        // Asian Payment Methods
-        "alipay",           // China
-        "wechat_pay",       // China
-        "grabpay",          // Southeast Asia
-        "paynow",           // Singapore
-        "promptpay",        // Thailand
-        "fpx",              // Malaysia
-        "konbini",          // Japan
-        
-        // Americas
-        "cashapp",          // US
-        "boleto",           // Brazil
-        "oxxo",             // Mexico
-        
-        // Oceania
-        "zip",              // Australia/NZ
-        
-        // US Bank Payments
-        "us_bank_account",
-        "acss_debit",       // Canada
+        "card",             // Credit/Debit cards + Apple Pay (automatic on supported devices)
+        "paypal",           // PayPal
       ],
       line_items: [
         {
